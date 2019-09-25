56e4943ea1dfaefec6819c1f729026d2f672661e
DevMai90 <dmai90@yahoo.com>
2019-09-25 05:24:30 -0700

    Created Spinner component to appear as we are fetching a random word

--- src/components/gameDisplay/WordDisplay.js
+++ src/components/gameDisplay/WordDisplay.js
@@ -1,20 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
 import { connect } from 'react-redux';
 
-const WordDisplay = ({ secretWord }) => {
+const WordDisplay = ({
+  word: { secretWord, guessedLetters, wrongLetters }
+}) => {
+  const wordBlanks = secretWord.split('').map((item, index) => {
+    if (guessedLetters.indexOf(item) < 0) {
+      return (
+        <p key={index} className="p-3" style={{ fontSize: 24 }}>
+          <u>
+            <strong>?</strong>
+          </u>
+        </p>
+      );
+    }
+    return (
+      <p key={index} className="p-3" style={{ fontSize: 24 }}>
+        <u>
+          <strong>{item}</strong>
+        </u>
+      </p>
+    );
+  });
+
+  const displayGuesses = guessedLetters.map(item => {
+    if (wrongLetters.indexOf(item) >= 0) {
+      return <p className="text-danger">{item}</p>;
+    }
+
+    return <p className="text-success">{item}</p>;
+  });
+
   return (
-    <div>
+    <div className="container">
+      <hr />
+      <div className="d-flex justify-content-center">
+        {secretWord ? wordBlanks : <Spinner />}
+      </div>
       <h3>Which guesses are correct?</h3>
       <p>{secretWord}</p>
       {/* Check guessed letters against secretWord */}
       {/* If the letter appears in the secretWord then display it if guessed */}
+      {displayGuesses}
     </div>
   );
 };
 
+WordDisplay.propTypes = {
+  word: PropTypes.object.isRequired
+};
+
 const mapStateToProps = state => ({
-  secretWord: state.word.secretWord
+  word: state.word
 });
 
 export default connect(mapStateToProps)(WordDisplay);
---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className="text-center">
+      <div className="spinner-border"></div>
+      <p>
+        <small>Please wait while we find the perfect word...</small>
+      </p>
+    </div>
+  );
+};
+
+export default Spinner;
