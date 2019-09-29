72aef8e3b7ded44ebb2ce41bc533c82df048a814
DevMai90 <dmai90@yahoo.com>
2019-09-29 12:20:12 -0700

    Updates

---
+++ precache-manifest.65979c468081a64c9b575d514cd50f8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d276204d004e23fd1b6bc3d91012a3e0",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "92251747016b2ae34778",
+    "url": "/hangman/static/css/main.9e935ba6.chunk.css"
+  },
+  {
+    "revision": "209273282707ce718337",
+    "url": "/hangman/static/js/2.98fc78c2.chunk.js"
+  },
+  {
+    "revision": "92251747016b2ae34778",
+    "url": "/hangman/static/js/main.496b11d4.chunk.js"
+  },
+  {
+    "revision": "46294ef07863907f63ec",
+    "url": "/hangman/static/js/runtime-main.67dd2d85.js"
+  }
+]);
